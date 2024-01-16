f180ec2df073607f0c8bcad5d926c73f8616115b
Nicola <156682869+NickPennik@users.noreply.github.com>
2024-01-16 18:31:20 +0000

    .

--- ex13.c
+++ ex13.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 int main(){
 
+int a;
 
+printf("quando sei nato?\n");
+scanf(" %d", &a);
+
+               if(a>1969){
+                  printf("sei nato/a %d", a-1969);
+                  printf("anni dopo il primo passo dell'uomo sulla Luna\n");
+               }
+else if(a<1969) {
+    printf("sei nato/a %d", 1969-a);
+    printf("anni prima del primo passo dell'uomo sulla Luna\n");
+}
+else {
+    printf("sei nato lo stesso anno del primo passo dell'uomo sulla Luna\n");
+}
 
 
     return 0;
