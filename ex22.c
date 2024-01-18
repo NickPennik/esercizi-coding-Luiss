de2fa0a7d653cc353e9477aa639c7e6d213887cd
Nicola <156682869+NickPennik@users.noreply.github.com>
2024-01-18 20:11:51 +0000

    sera

---
+++ ex22.c
@@ -0,0 +1,37 @@
+// il programma prende due valori e una funzione deve inverire il valore con il secondo e viceversa
+
+#include <stdio.h>
+
+int inverti()
+{
+    int x;
+    int y;
+    int i = 0;
+
+    printf("assegnare due valori alle variabili x e y\n");
+    scanf("%d\n%d", &x, &y);
+    printf("invertiamo il primo valore con il secondo\n");
+    printf("%d\n", y);
+}
+
+int inverti2()
+{
+    int x;
+    int y;
+    
+    printf("ed ora il secondo con il primo\n");
+    printf("%d\n", x);
+}
+
+int main()
+{
+    int i = 0;
+
+    while (i<1)
+    {
+        i= i+1;
+        inverti();
+    }
+    inverti2();
+    return 0;
+}
