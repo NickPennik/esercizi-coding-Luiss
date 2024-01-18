f9fcb590d4170de4a12628cb037a9d0e9877ca24
Nicola <156682869+NickPennik@users.noreply.github.com>
2024-01-18 09:24:45 +0000

    20 e 21

---
+++ ex20.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    float n;
+    float den;
+    float temp;
+    float somma;
+
+    den = 1;
+    temp = 0;
+    n = 0;
+    printf("Inserisci un numero: ");
+    scanf("%f", &n);
+    while (n > 0){
+        somma = n + temp;
+        printf("La media è %f \n",somma / den);
+        temp= somma;
+        den = den + 1;
+        printf("Inserisci un numero: ");
+        scanf("%f", &n);  
+    }
+    printf("Questo numero è negativo, mi dispiace non posso fare la media \n");
+    return 0;
+}
