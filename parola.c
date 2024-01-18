6e31c100337240b017c3aa77c7750c53f7a25518
Nicola <156682869+NickPennik@users.noreply.github.com>
2024-01-18 13:57:52 +0000

    parola

--- parola.c
+++ parola.c
@@ -3,10 +3,14 @@
 
 int main()
 {
-    char parola [5]={'c','i','a','o','!'};
-    char parola2 [5]={'c','i','\0','o','!'};
+    char parola [5]={'c','i','a','o','\0'};
+    char parola2 [5]={'c','i','\0','o','!'}; 
+    char parola3 []="vamo raga forza giuve";
+    
+    // il \0 mi tronca la parola poiché indica quando la stringa termina
 
     printf("%s\n", parola);
     printf("%s\n", parola2);
+    printf("%s\n", parola3);
     return 0;
 }
